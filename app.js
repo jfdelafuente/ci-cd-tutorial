32622b30f1c8dea6c13ac83a815bfe9ab9b4b4c1
jfdelafuente <josefcodelafuente@gmail.com>
2024-12-09 19:13:52 +0100

    feat: mod texto app

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.get("/", (req, res, next) => {
       </head>
     </html>
     <body>
-      <h1>Hello world! I'm a Node/Express Js web server...</h1>
+      <h1>Hello world! I'm a Node/Express Js web server... deployed in GCP</h1>
     </body>
     `);
   next();
